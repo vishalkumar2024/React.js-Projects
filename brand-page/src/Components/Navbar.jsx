2209d57590682793c718031b04bc32ad3d4e7b1d
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-03-03 17:40:33 +0530

    added Navbar.jsx component

--- brand-page/src/Components/Navbar.jsx
+++ brand-page/src/Components/Navbar.jsx
@@ -3,17 +3,19 @@ import React from 'react'
 export default function Navbar() {
   return (
     <>
-      <div className='bg-gray-300'>
-        <div></div>
+      <div className=" h-18 flex items-center justify-around">
         <div>
-          <ul>
+          <img src="https://images.seeklogo.com/logo-png/9/1/nike-logo-png_seeklogo-99478.png" className="h-20 w-25 " alt="Logo mage" />
+        </div>
+        <div className=''>
+          <ul className='flex h-20 gap-4 items-center text-[18px] font-semibold'>
             <li>Menu</li>
             <li>Location</li>
             <li>About</li>
             <li>Contact</li>
           </ul>
         </div>
-        <div></div>
+        <button className='bg-red-700 text-white rounded py-2 px-4'>Login</button>
       </div>
     </>
   )
