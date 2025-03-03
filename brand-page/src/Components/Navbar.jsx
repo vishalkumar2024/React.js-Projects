d7a15403f32bacd17672e268ace88fb2d9dd6f52
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-03-03 17:16:32 +0530

    modified

--- brand-page/src/Components/Navbar.jsx
+++ brand-page/src/Components/Navbar.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 export default function Navbar() {
   return (
     <>
-      <div className='bg-gray-300'>The Navbar</div>
+      <div className='bg-gray-300'>
+        <div></div>
+        <div>
+          <ul>
+            <li>Menu</li>
+            <li>Location</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div></div>
+      </div>
     </>
   )
 }
